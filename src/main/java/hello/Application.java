1823ec9f17e0f356c8f41db00e0b0f06b52d1c1d
brampel <brampel23@gmail.com>
2019-05-04 17:01:13 -0400

    put instructions to run web app in application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,9 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    /*
+     * main method for web app
+     * currently to test this you must
+         * spring-boot:run in maven
+         * localhost:8080/startGame
+         * localhost:8080/game?player=John (or a name of a player in the game)
+     */
     public static void main(String[] args) {
-
-
         SpringApplication.run(Application.class, args);
     }
 }
